4ba7e05f776256c3526be8bf5c5060c713e4ce77
Dushman <ebanniynasos@gmail.com>
2014-10-24 09:42:29 +0300

    loader added

--- js/script.js
+++ js/script.js
@@ -4,14 +4,22 @@ $(function(){
     minSlides: 1,
     maxSlides: 1,
     nextText: 'B',
-    prevText: 'A'
+    prevText: 'A',
+    onSliderLoad: function(){
+      $('#index-top-slider .slide').css('visibility' , 'visible'); 
+      $('.slider-loader').remove();
+    }
   });
 
   $('.index-bottom-slider').bxSlider({
     minSlides: 1,
     maxSlides: 1,
     nextText: 'B',
-    prevText: 'A'
+    prevText: 'A',
+    onSliderLoad: function(){
+      $('#index-bottom-slider .slide').css('visibility' , 'visible'); 
+      $('.slider-loader').remove();
+    }
   });
 
 });
