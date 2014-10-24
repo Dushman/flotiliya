f4b8ae0c85940c4bd099d7950a340d69d9d9dafc
Dushman <ebanniynasos@gmail.com>
2014-10-24 12:51:12 +0300

    1

--- js/script.js
+++ js/script.js
@@ -22,59 +22,12 @@ $(function(){
     }
   });
 
-  /*function initialize() {
-    var mapOptions = {
-      zoom: 14,
-      scrollwheel: false,
-      draggable: false,
-      disableDefaultUI: true,
-      center: new google.maps.LatLng(55.8553, 37.4765)
-    }
-
-    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-                                  
-  }
-
-  function attachMessage(marker, message) {
-        
-    var infowindow = new google.maps.InfoWindow({
-      content: message,
-      maxWidth: 200
-    });
-
-    infowindow.open(marker.get('map'), marker);
-
-  }
-    
-  google.maps.event.addDomListener(window, 'load', initialize);*/
-
-
-
-
-
-
-
   function initialize() {
-    // Create an array of styles.
+  
     var styles = [
-      {
-        featureType: "all",
-        stylers: [
-          { visibility:"off"}
-        ]
-      },{
-        featureType: "road",
-        stylers: [
-          { hue: "#9fd4ff" },
-          {visibility:"on"}
-        ]
-      },{
-        featureType: "road",
-        elementType: "labels",
-        stylers: [
-          { visibility: "off" }
-        ]
-      }
+      { "stylers": [ { "color": "#ffffff" } ] },
+      { "featureType": "road", "stylers": [ { "color": "#9fd4ff" } ] },
+      { "elementType": "labels", "stylers": [ { "visibility": "off" } ] }
     ];
 
     var styledMap = new google.maps.StyledMapType(styles,
@@ -85,6 +38,11 @@ $(function(){
       scrollwheel: false,
       draggable: false,
       disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.RIGHT_CENTER
+      },
       center: new google.maps.LatLng(55.8553, 37.4765),
       mapTypeControlOptions:{
         mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
