54b2f94cde51d808ab826c136c4a5fb62e9c82bc
Dushman <ebanniynasos@gmail.com>
2014-10-24 11:07:48 +0300

    map added

--- js/script.js
+++ js/script.js
@@ -22,4 +22,94 @@ $(function(){
     }
   });
 
+  /*function initialize() {
+    var mapOptions = {
+      zoom: 14,
+      scrollwheel: false,
+      draggable: false,
+      disableDefaultUI: true,
+      center: new google.maps.LatLng(55.8553, 37.4765)
+    }
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+                                  
+  }
+
+  function attachMessage(marker, message) {
+        
+    var infowindow = new google.maps.InfoWindow({
+      content: message,
+      maxWidth: 200
+    });
+
+    infowindow.open(marker.get('map'), marker);
+
+  }
+    
+  google.maps.event.addDomListener(window, 'load', initialize);*/
+
+
+
+
+
+
+
+  function initialize() {
+    // Create an array of styles.
+    var styles = [
+      {
+        featureType: "all",
+        stylers: [
+          { visibility:"off"}
+        ]
+      },{
+        featureType: "road",
+        stylers: [
+          { hue: "#9fd4ff" },
+          {visibility:"on"}
+        ]
+      },{
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          { visibility: "off" }
+        ]
+      }
+    ];
+
+    var styledMap = new google.maps.StyledMapType(styles,
+      {name: "Styled Map"});
+
+    var mapOptions = {
+      zoom: 12,
+      scrollwheel: false,
+      draggable: false,
+      disableDefaultUI: true,
+      center: new google.maps.LatLng(55.8553, 37.4765),
+      mapTypeControlOptions:{
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+      }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'),
+      mapOptions);
+
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
+
+  }
+
+  function attachMessage(marker, message) {
+        
+        var infowindow = new google.maps.InfoWindow({
+          content: message,
+          maxWidth: 200
+        });
+
+        infowindow.open(marker.get('map'), marker);
+
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 });
